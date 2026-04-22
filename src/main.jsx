478f36b9885a0b7f54e4b0f95b5baa58125da007
Ferdous <ferdous1996h@gmail.com>
2026-04-22 22:42:54 +0600

    fix case

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import App from './App';
 import { createRoot } from 'react-dom/client';
 import { ErrorBoundary } from 'react-error-boundary';
-import { ErrorFallback } from './components/test';
+import { ErrorFallback } from './components/ErrorFallback';
 createRoot(document.querySelector('#root')).render(
   <ErrorBoundary FallbackComponent={ErrorFallback}>
     <App />
