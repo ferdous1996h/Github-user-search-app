13891e678502c9338ae5a8d545174b1496034619
Ferdous <ferdous1996h@gmail.com>
2026-04-22 22:41:53 +0600

    tempRename

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import App from './App';
 import { createRoot } from 'react-dom/client';
 import { ErrorBoundary } from 'react-error-boundary';
-import { ErrorFallback } from './components/ErrorFallback';
+import { ErrorFallback } from './components/test';
 createRoot(document.querySelector('#root')).render(
   <ErrorBoundary FallbackComponent={ErrorFallback}>
     <App />
